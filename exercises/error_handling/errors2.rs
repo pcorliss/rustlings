113d121946bdd0dc9c33bf04fc13e224a96c71aa
Philip Corliss <pcorliss@50projects.com>
2020-05-27 21:52:35 +0000

    Progress!

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -16,16 +16,33 @@
 // There are at least two ways to implement this that are both correct-- but
 // one is a lot shorter! Execute `rustlings hint errors2` for hints to both ways.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+
+    // 1st Working Attempt
+    // let qty = item_quantity.parse::<i32>();
+    // if qty.is_err() {
+    //     return qty;
+    // }
+    //
+    // Ok(qty.unwrap() * cost_per_item + processing_fee)
+
+    // 2nd Working Attempt
+    // Hint suggested the `?` operator
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
+
+    // 3rd Working Attempt
+    // According to the hint this is very common pattern. But the `?` operator was designed to
+    // handle it.
+    // match item_quantity.parse::<i32>() {
+    //     Ok(qty) => Ok(qty * cost_per_item + processing_fee),
+    //     Err(e) => Err(e),
+    // }
 }
 
 #[cfg(test)]
@@ -40,8 +57,8 @@ mod tests {
     #[test]
     fn item_quantity_is_an_invalid_number() {
         assert_eq!(
-            total_cost("beep boop").unwrap_err().to_string(),
-            "invalid digit found in string"
+           total_cost("beep boop").unwrap_err().to_string(),
+           "invalid digit found in string"
         );
     }
 }
